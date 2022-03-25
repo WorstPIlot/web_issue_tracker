d9b7a303ac6bbf41ab2625c2adc5f63493ed38d7
Denis Stepulenok <super.denis@gmail.com>
2022-03-25 19:49:01 +0300

    [!] Теперь issues показываются в таблице (#4)

--- mysite/mysite/models.py
+++ mysite/mysite/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Issues(models.Model):
-    issue_id = models.IntegerField()
+    issue_id = models.IntegerField(primary_key=True)
     owner_id = models.IntegerField()
     public = models.IntegerField()
     tags = models.CharField(max_length=254)
