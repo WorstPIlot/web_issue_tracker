8c9930f31e7f07e91e1c538e7092fc39708d2447
Denis Stepulenok <super.denis@gmail.com>
2022-04-15 20:15:15 +0300

    [+] Приоритеты загружаются из базы данных (#5)

--- document/models.py
+++ document/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 
-# Create your models here.
+# Возможные приоритеты хранятся в Базе Данных
+class Priority(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=16)
+
+
 class Document(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True, null=True)
-    priority = models.IntegerField(default=0)
+    priority = models.ForeignKey(Priority, on_delete=models.RESTRICT,
+                                 db_column='priority')
 
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
